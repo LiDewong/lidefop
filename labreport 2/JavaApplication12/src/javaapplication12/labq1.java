23d64c44e72292d8243403e9398e63fe70f3d668
LiDewong <95974704+LiDewong@users.noreply.github.com>
2021-12-21 22:31:16 +0800

    Create labq1.java

---
+++ labreport 2/JavaApplication12/src/javaapplication12/labq1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication12;
+
+/**
+ *
+ * @author User
+ */
+import java.util.Scanner;
+public class labq1 {
+    pulic static double findmedian(int array){
+        double median=0;
+        return median;
+    }
+    public static void main(String[] args) {
+        int[] array = new int[]
+                
+        findmedian(array[]);
+    }
+    
+}
